94ea505728032bd77d9996e2f5bae8e352be518e
tzvaita <tzvaita@gmail.com>
2020-10-22 12:30:47 +0200

    Implement BooksList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import BooksList from './containers/BooksList'
-import BooksForm from './containers/BooksForm'
+import BooksList from './containers/BooksList';
+// import BooksForm from './containers/BooksForm'
 
 export default function App() {
   return (
     <div className="App">
       <BooksList />
-      <BooksForm />
+      {/* <BooksForm /> */}
     </div>
   );
 }
---
+++ src/containers/BooksList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
+
+const BooksList = ({ books }) => {
+  const booksList = books.map(book => <Book book={book} key={Math.random()} />);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {booksList}
+      </tbody>
+    </table>
+  );
+};
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+const mapStateToProps = state => (
+  {
+    books: state.books,
+  }
+);
+export default connect(mapStateToProps)(BooksList);
