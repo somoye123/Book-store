70ec9f794a81d7b9435a7b4c536974a49727736d
somoye123 <somoye.ayotunde@gmail.com>
2020-10-22 12:02:42 +0100

    add a constant module in common folder

---
+++ src/common/constantModule.js
@@ -0,0 +1,8 @@
+const constant = () => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  return {
+    categories,
+  };
+};
+
+export default constant();
