0c75cc9b8a08b79d129bfc7d27141b5c190643c2
somoye123 <somoye.ayotunde@gmail.com>
2020-10-22 11:50:36 +0100

    implement book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,1 +1,20 @@
-console.log('HI');
+import React from 'react';
+
+const Book = ({ book }) => {
+  const { id, title, category } = book;
+  return (
+    <tr>
+      <td>
+        {id}
+      </td>
+      <td>
+        {title}
+      </td>
+      <td>
+        {category}
+      </td>
+    </tr>
+  );
+};
+
+export default Book;
