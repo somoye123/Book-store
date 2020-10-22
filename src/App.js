8f0ac06c342beab222001a1dd118c3820d97a9fb
tzvaita <tzvaita@gmail.com>
2020-10-22 11:22:47 +0200

    Implement the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import BooksList from './containers/BooksList'
+import BooksForm from './containers/BooksForm'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello from Tennyson and Somoye</h1>
+      <BooksList />
+      <BooksForm />
     </div>
   );
 }
