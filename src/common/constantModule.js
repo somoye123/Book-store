e01f8b5862e12f7b2b1879f51c20f7453e8a483d
somoye123 <somoye.ayotunde@gmail.com>
2020-10-23 09:15:31 +0100

    Enhance Redux store to keep track of current books filter

--- src/common/constantModule.js
+++ src/common/constantModule.js
@@ -1,17 +1,21 @@
 const constant = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const defaultState = [
-    {
-      id: Math.floor(Math.random() * 1000),
-      title: 'Somoye and his projects',
-      category: 'Action',
-    },
-    {
-      id: Math.floor(Math.random() * 1000),
-      title: 'Tennyson and his projects',
-      category: 'History',
-    },
-  ];
+  const generateRandomId = () => Math.floor(Math.random() * 1000);
+  const defaultState = {
+    books: [
+      {
+        id: generateRandomId(),
+        title: 'Somoye and his projects',
+        category: 'Action',
+      },
+      {
+        id: generateRandomId(),
+        title: 'Tennyson and his projects',
+        category: 'History',
+      },
+    ],
+    filter: 'All',
+  };
   return {
     categories,
     defaultState,
